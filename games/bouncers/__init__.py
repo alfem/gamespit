3e9723f09ad2a2dbd8f184f234cff6b0a1e69af0
Alfonso E.M <alfonso@el-magnifico.org>
2013-03-05 14:59:23 +0100

    Mouse support added. Launch of games working!

---
+++ games/bouncers/__init__.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+# Bouncers
+
+# Author: Alfonso E.M. <alfonso@el-magnifico.org>
+# License: Free (GPL2) 
+# Version: 1.0 - 27/Feb/2013
+
+from Game import Game
+
+class Menu(Game):
+
+    def start(self):
+      self.x=400
+      self.y=10
+      self.ix=1
+      self.iy=1
+
+
+    def loop(self):
+
+      animals=("cat","cow","duck")
+      which=0
+
+      while True:
+          self.x=self.x+self.ix
+          self.y=self.y+self.iy
+          if self.x > self.DISPLAY.width-150 or self.x < 0:
+              self.ix=-self.ix
+
+          if self.y > self.DISPLAY.height-150:
+             self.iy=-self.iy
+
+          self.iy=float(self.iy+0.01)
+
+          self.fill()
+          self.DISPLAY.print_image(self.IMAGES[animals[which]],self.x, self.y)
+          self.DISPLAY.show()
+
+          user_input=self.CONTROLLER.check_user_action()
+
+          controller_type=user_input[0] 
+
+          if controller_type == "K": #Keyboard
+              key_name,key_modifiers=user_input[1:]
+
+              if key_name:
+                which +=1
+                if which == len(animals):
+                    which = 0
+          self.wait(6)
+
+
+# Main
+def main(CONF, DISPLAY, CONTROLLER):
+
+    menu=Menu(CONF,DISPLAY,CONTROLLER)
+    menu.start()
+    menu.loop()
+
--- games/menu/__init__.py
+++ games/menu/__init__.py
@@ -13,19 +13,22 @@ from configobj import ConfigObj
 
 from Game import Game
 
+
 class MenuItem:
-    def __init__(self,base_path):
+    def __init__(self, name, base_path):
         conf = ConfigObj(os.path.join(base_path,"game.conf"))
-        self.title=conf["GAME"]["title"]
+        self.name=name
+        self.title=conf["GAME"]["title"] #unused currently
         self.screenshot=pygame.image.load(os.path.join(base_path,"screenshot.png"))
         self.base_path=base_path
+
 
 class Menu(Game):
 
     def start(self):
       self.DISPLAY.print_text("GAMES PIT",1,1,self.FONTS["FreeSans80"], self.COLORS['title'])
 
-      self.menuitems=[]
+      self.menu_items=[]
       self.index=0
 
       games_dir_name=os.path.join(self.CONF["GAME"]["base_path"],"..")
@@ -33,14 +36,26 @@ class Menu(Game):
           dir_list=os.listdir(games_dir_name)
           for game in dir_list:
               if game != "menu":
-                  self.menuitems.append(MenuItem(os.path.join(games_dir_name,game))) 
+                  self.menu_items.append(MenuItem(game, os.path.join(games_dir_name,game))) 
  
 
+# Converts screen index to screen coordinates
     def i2coords(self, offset,i):
         index=offset+i
         l=i / 3
         c=i % 3
         return index,l,c
+
+# Converts screen coordinates to index of menu items 
+    def coords2index(self, offset, coords):
+        for i in range(9):
+            index,l,c=self.i2coords(offset,i)
+            x=c*250+50
+            y=l*190+40
+            box=pygame.Rect(x,y,200,150)
+            if box.collidepoint(coords): 
+                return offset+i
+        return False 
 
 
     def loop(self):
@@ -52,14 +67,13 @@ class Menu(Game):
       while True:
           self.fill()
 
-          for i in range(0,9):
+          for i in range(9):
               index,l,c=self.i2coords(offset,i)
-              print l,c,index
-              if index < len(self.menuitems):
+              if index < len(self.menu_items):
                   x=c*250+50
                   y=l*190+40
-                  self.DISPLAY.print_image(self.menuitems[index].screenshot, x, y)
-                  self.DISPLAY.print_text(self.menuitems[index].title, x, y,self.FONTS['FreeSans20'],self.COLORS['title'])
+                  self.DISPLAY.print_image(self.menu_items[index].screenshot, x, y)
+                  self.DISPLAY.print_text(self.menu_items[index].title, x, y,self.FONTS['FreeSans20'],self.COLORS['title'])
 
 # LOOP THRU ITEMS IN A PAGE
           while True:  
@@ -70,36 +84,49 @@ class Menu(Game):
                                   
               self.DISPLAY.show()        
 
-              key_name,key_modifiers=self.CONTROLLER.wait_for_user_action()
+              user_input=self.CONTROLLER.wait_for_user_action()
 
               self.DISPLAY.print_image(buffer, x, y)
 
-              if key_name == 'right' and c < 2:
-                selected_index += 1
-              if key_name == 'left' and c > 0:
-                selected_index -= 1
-              if key_name == 'up':
-                  if l == 0:
-                      if offset > 0:
-                          offset=offset - 9
-                          selected_index += 6
-                          break
-                  else:
-                      selected_index -= 3
-              if key_name == 'down':
-                  if l == 2:
-                      if offset < len(self.menuitems) - 9: 
-                          offset=offset + 9
-                          selected_index -= 6
-                          break
-                  else:
-                      selected_index += 3
+              controller_type=user_input[0] 
 
+              if controller_type == "K": #Keyboard
+                  key_name,key_modifiers=user_input[1:]
+                  if key_name == 'right' and c < 2:
+                    selected_index += 1
+                  if key_name == 'left' and c > 0:
+                    selected_index -= 1
+                  if key_name == 'up':
+                      if l == 0:
+                          if offset > 0:
+                              offset=offset - 9
+                              selected_index += 6
+                              break
+                      else:
+                          selected_index -= 3
+                  if key_name == 'down':
+                      if l == 2:
+                          if offset < len(self.menu_items) - 9: 
+                              offset=offset + 9
+                              selected_index -= 6
+                              break
+                      else:
+                          selected_index += 3
+                  if key_name == 'return':
+                      return self.menu_items[index].name
+
+
+              if controller_type == "M": #Mouse
+                  coords,button=user_input[1:]  
+                  index=self.coords2index(offset, coords)
+                  if index:
+                      return self.menu_items[index].name
 
 # Main
 def main(CONF, DISPLAY, CONTROLLER):
 
     menu=Menu(CONF,DISPLAY,CONTROLLER)
     menu.start()
-    menu.loop()
+    game_to_launch=menu.loop()
+    return game_to_launch
 
